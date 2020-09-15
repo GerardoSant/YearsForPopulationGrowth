189bc664317e0094ba3574f403b47eb22d6bd7a6
Helio Machado <0x2b3bfa0+git@googlemail.com>
2020-09-15 16:11:18 +0200

    Add draft Javadoc comment

--- src/main/java/YearsForPopulationGrowth.java
+++ src/main/java/YearsForPopulationGrowth.java
@@ -1,6 +1,27 @@
 import java.lang.Math;
 
 public class YearsForPopulationGrowth {
+    /**
+     * Calculates the number of whole years required to surpass
+     * the provided population with a steady annual growth rate
+     *
+     * @param initialPopulation           Initial population, must be greater
+     *                                    than 0 to calculate any growth time
+     *
+     * @param annualIncreasePercentage    Yearly population growth percentage
+     *                                    100 duplicates population in a year
+     *
+     * @param populationToSurpass         Population to surpass. The function
+     *                                    will return 0 if initial population
+     *                                    is already greater than this amount
+     *
+     * @return                            -1 for initial populations that are
+     *                                    less or equal to 0, exactly 0 given
+     *                                    an initial population exceeding the
+     *                                    population to surpass, or any other
+     *                                    positive integer when none of these
+     *                                    conditions are met
+     */
     public static int of(
         double initialPopulation,
         double annualIncreasePercentage,
