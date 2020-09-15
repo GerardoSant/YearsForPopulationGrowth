25cfbdb5cb15e02cfcb6f93b65d66457727d9929
Helio Machado <0x2b3bfa0+git@googlemail.com>
2020-09-15 22:30:13 +0200

    Enforce code style with prettier.io

--- src/test/java/YearsForPopulationGrowth_.java
+++ src/test/java/YearsForPopulationGrowth_.java
@@ -6,16 +6,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class YearsForPopulationGrowth_ {
 
     /* Example invocation:
-     * 
+     *
      * YearsForPopulationGrowth.of(
      *     initialPopulation,
      *     increasingPercentagePerYear,
      *     populationToSurpass
      * );
      */
-    
+
     @Test
-    public void should_return_minus1_given_initialBelow1(){
+    public void should_return_minus1_given_initialBelow1() {
         assertThat(YearsForPopulationGrowth.of(0, 10, 1000)).isEqualTo(-1);
         assertThat(YearsForPopulationGrowth.of(0, 35, 300)).isEqualTo(-1);
         assertThat(YearsForPopulationGrowth.of(0, 25, 400)).isEqualTo(-1);
@@ -24,41 +24,39 @@ public class YearsForPopulationGrowth_ {
     }
 
     @Test
-    public void should_return_0_given_surpassBelowInitial(){
+    public void should_return_0_given_surpassBelowInitial() {
         assertThat(YearsForPopulationGrowth.of(1000, 10, 999)).isEqualTo(0);
         assertThat(YearsForPopulationGrowth.of(2000, 10, 800)).isEqualTo(0);
         assertThat(YearsForPopulationGrowth.of(32000, -10, -100)).isEqualTo(0);
     }
 
-
     @Test
-    public void should_return_1_given_initial1000_percentage10_surpass1000(){
+    public void should_return_1_given_initial1000_percentage10_surpass1000() {
         assertThat(YearsForPopulationGrowth.of(1000, 10, 1000)).isEqualTo(1);
     }
 
     @Test
-    public void should_return_1_given_initial1000_percentage10_surpass1099(){
+    public void should_return_1_given_initial1000_percentage10_surpass1099() {
         assertThat(YearsForPopulationGrowth.of(1000, 10, 1099)).isEqualTo(1);
     }
 
     @Test
-    public void should_return_2_given_initial1000_percentage10_surpass1100(){
+    public void should_return_2_given_initial1000_percentage10_surpass1100() {
         assertThat(YearsForPopulationGrowth.of(1000, 10, 1100)).isEqualTo(2);
     }
 
-
     @Test
-    public void should_return_2_given_initial1000_percentage10_surpass1209(){
+    public void should_return_2_given_initial1000_percentage10_surpass1209() {
         assertThat(YearsForPopulationGrowth.of(1000, 10, 1209)).isEqualTo(2);
     }
 
     @Test
-    public void should_return_3_given_initial1000_percentage10_surpass1210(){
+    public void should_return_3_given_initial1000_percentage10_surpass1210() {
         assertThat(YearsForPopulationGrowth.of(1000, 10, 1210)).isEqualTo(3);
     }
 
     @Test
-    public void should_return_5_given_initial1000_percentage10_surpass1609(){
+    public void should_return_5_given_initial1000_percentage10_surpass1609() {
         assertThat(YearsForPopulationGrowth.of(1000, 10, 1609)).isEqualTo(5);
     }
 }
